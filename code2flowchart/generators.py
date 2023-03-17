fd65acc0235d80db6a250fbd8000e7c1bc7f9404
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-03-17 15:12:00 +0100

    refactor: remove extra files

--- code2flowchart/generators.py
+++ code2flowchart/generators.py
@@ -3,6 +3,7 @@ from langchain.llms import OpenAI
 from utils.mermaid import generate_flowchart
 from templates.prompt import flowchart_template
 
+
 def generate_output(code):
     llm = OpenAI(temperature=0.9)
 
